6eedbf64bc8a4fc8604a9925939e4a9150729739
MoritzEichhorn <moritz.eichhorn@outlook.com>
2021-10-21 14:25:56 +0200

    Add html form

--- backend/src/main/java/com/fit/boundary/EntryResource.java
+++ backend/src/main/java/com/fit/boundary/EntryResource.java
@@ -4,12 +4,13 @@ import com.fit.controller.EntryRepository;
 import com.fit.entities.Entry;
 
 import javax.inject.Inject;
+import javax.transaction.Transactional;
 import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
-import java.time.Instant;
 
 @Path("/api/entry")
+@Transactional
 public class EntryResource {
     @Inject
     EntryRepository entryRepository;
@@ -23,7 +24,6 @@ public class EntryResource {
                                  @FormParam("telephoneNo") String telephoneNo) {
         Entry e = new Entry(null, firstName, lastName, email, telephoneNo);
         entryRepository.persist(e);
-
         return Response.status(201).entity(e).build();
     }
 }
--- backend/src/main/java/com/fit/controller/EntryRepository.java
+++ backend/src/main/java/com/fit/controller/EntryRepository.java
@@ -3,5 +3,8 @@ package com.fit.controller;
 import com.fit.entities.Entry;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
+import javax.enterprise.context.ApplicationScoped;
+
+@ApplicationScoped
 public class EntryRepository implements PanacheRepository<Entry> {
 }
